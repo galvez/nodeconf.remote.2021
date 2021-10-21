9e75e02038a775f10d62188a5140f6ff4d442b17
Jonas Galvez <jonasgalvez@gmail.com>
2021-10-21 14:39:49 -0300

    fix

--- fastify/get-data/entry/client.js
+++ fastify/get-data/entry/client.js
@@ -2,7 +2,7 @@ import { createApp } from '../main'
 import { hydrate } from 'fastify-vite-vue/client'
 const { app, router } = createApp()
 
-hydrate(app)
-
-// Wait until router is ready before mounting to ensure hydration match
-router.isReady().then(() => app.mount('#app'))
+hydrate(app).then(() => {
+  // Wait until router is ready before mounting to ensure hydration match
+  router.isReady().then(() => app.mount('#app'))
+})
